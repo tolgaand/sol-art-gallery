e2123ca6defdb2ebd376d87d5be4010a13435f4c
tolgaand <tolgaand@yandex.com>
2022-12-19 04:20:22 +0300

    fix: add border top radius to image

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -15,7 +15,15 @@ export const ImageCard = ({ image }: ImageCardProps) => {
   return (
     <Box key={image.id} backgroundColor="#607DE6" borderRadius="30px">
       <Link target="_blank" href={image.url}>
-        <LazyLoadImage src={image.url} alt={image.prompt} effect="blur" />
+        <LazyLoadImage
+          src={image.url}
+          alt={image.prompt}
+          effect="blur"
+          style={{
+            borderTopLeftRadius: "30px",
+            borderTopRightRadius: "30px",
+          }}
+        />
       </Link>
 
       <Stack padding="20px">
