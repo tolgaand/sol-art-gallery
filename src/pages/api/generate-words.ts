96e3a09798b4c8ba0d762dbedeebd355e9a0c537
tolgaand <tolgaand@yandex.com>
2022-12-22 23:30:51 +0300

    feat: add the generate random sentence with openai

--- src/pages/api/generate-words.ts
+++ src/pages/api/generate-words.ts
@@ -1,4 +1,5 @@
 import { generateWords } from "lib/generateWords";
+import openai from "lib/openai";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -9,5 +10,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ words: generateWords(5) });
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Make meaningful, descriptive sentences.",
+    max_tokens: 10,
+    temperature: 0.9,
+  });
+
+  const sentence = response.data.choices[0].text?.trim() || generateWords(5);
+
+  res.status(200).json({ words: sentence });
 }
